3cf32773fda92f5fa6ca21d6ac4f50ea96564b40
c04o <c04o@proton.me>
2025-05-28 07:32:35 -0700

    Update deque.py

--- deque.py
+++ deque.py
@@ -1,1 +1,19 @@
+# historial_tramites = {}
 
+historial_tramites = {}
+
+def registrar_tramite(ciudadano, tramite):
+    
+    if ciudadano not in historial_tramites:
+        historial_tramites[ciudadano] = [] 
+    historial_tramites[ciudadano].append(tramite)
+    print(f"Trámite '{tramite}' registrado para {ciudadano}.")
+
+def ver_historial(ciudadano):
+
+    if ciudadano in historial_tramites and historial_tramites[ciudadano]:
+        print(f"Historial de trámites de {ciudadano}:")
+        for tramite in reversed(historial_tramites[ciudadano]):
+            print(f"- {tramite}")
+    else:
+        print(f"No hay historial de trámites para {ciudadano}.")
